f955dcf50ccab3f65336afe4fbdc44a0783ec19c
trkzmn <trkzmn89@gmail.com>
2020-09-21 11:34:08 +0000

    msm8916-common: Implement health 2.0 hal

--- msm8916.mk
+++ msm8916.mk
@@ -122,6 +122,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
+
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
