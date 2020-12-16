cca1a04b4c7825284d9cc71f356333535847be29
jro1979oliver <jroliveira_oliveira301@gmail.com>
2020-12-16 19:36:00 +0100

    msm8916-common: Build just sysfs livedisplay service
    * Get rid of proprietary stuff;
    * Remove ISunlightEnhancement.
    
    Change-Id: Ibd1fc327d8d3ed58f988b05ea052d97ae3cea6c2

--- msm8916.mk
+++ msm8916.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
